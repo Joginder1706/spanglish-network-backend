13a4367b64b7e6ffb1730394f825499b630c0732
Joginder1706 <joginder@trustycoders.com>
2024-11-14 17:04:09 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,15 +1,45 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+// import mysql from 'mysql2/promise';
+// import dotenv from 'dotenv';
 
+// dotenv.config();
+
+// const db = await mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// });
+
+// console.log("Connected to MySQL database");
+
+// export default db;
+
+
+import mysql from "mysql2";
+import dotenv from "dotenv";
 dotenv.config();
 
-const db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+// Create a connection to the database
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-console.log("Connected to MySQL database");
+// Pinging the database to keep the connection alive
+setInterval(() => {
+  pool.query("SELECT 1", (err) => {
+    if (err) {
+      console.error("Ping error:", err);
+    } else {
+      console.log("Database connected");
+    }
+  });
+}, 60000); // 60 seconds
 
-export default db;
+export default pool;
